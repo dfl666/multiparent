5f4cab686804b27aa03522db79070c86bd51fdee
dongfl <1075608781@qq.com>
2020-07-09 13:50:42 +0800

    dubbo-demo

--- consumer/src/main/java/com/hiram/dubbo/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/hiram/dubbo/consumer/ConsumerApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ConsumerApplication {
 
     public static void main(String[] args) {
+        System.out.println("111");
         SpringApplication.run(ConsumerApplication.class, args);
     }
 
