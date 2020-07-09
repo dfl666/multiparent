dcccfb8204a4746a1b95a937a60dc8c7237362a2
dongfl <1075608781@qq.com>
2020-07-09 13:47:47 +0800

    dubbo-demo

---
+++ consumer/src/main/java/com/hiram/dubbo/consumer/controller/DemoController.java
@@ -0,0 +1,22 @@
+package com.hiram.dubbo.consumer.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.hiram.dubbo.api.service.DemoService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @Reference(version = "${demo.service.version}")
+    private DemoService demoService;
+
+
+    @GetMapping("/say/{name}")
+    public String sayHello(@PathVariable  String name){
+        return demoService.sayHello(name);
+    }
+
+
+}
---
+++ provider/src/main/java/com/hiram/dubbo/provider/service/impl/DemoServiceImpl.java
@@ -0,0 +1,12 @@
+package com.hiram.dubbo.provider.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.hiram.dubbo.api.service.DemoService;
+
+@Service(version = "${demo.service.version}")
+public class DemoServiceImpl implements DemoService {
+    @Override
+    public String sayHello(String name) {
+        return "hello"+name+"i,m provider";
+    }
+}
